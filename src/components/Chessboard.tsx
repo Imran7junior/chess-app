a9cad241fd9adfab72efe68a29d305e399976cde
MD Imran Mansuri <md.imran.568@gmail.com>
2024-03-12 00:55:13 +0530

    added move hold drop piece functions

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Tile from "./tile/Tile";
 import './Chessboard.css';
+import { act } from "react-dom/test-utils";
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -15,7 +16,7 @@ interface Piece {
 const pieces: Piece[] = [];
 
 for(let p = 0; p < 2; p++){
-	const type = (p == 0) ? "b" : "w";
+	const type = (p === 0) ? "b" : "w";
 	const y = (p === 0) ? 7 : 0;
 
 
@@ -36,7 +37,40 @@ for (let i = 0; i <  8; i++){
 	pieces.push({image:  "assets/images/pawn_w.png", x:i, y:1})
 }
 
+let activePiece: HTMLElement | null = null;
 
+function grabPiece(e: React.MouseEvent<HTMLDivElement, MouseEvent>){
+	const element = e.target as HTMLElement;
+	
+	if(element.classList.contains("chess-piece")) {
+		console.log(e);
+		
+		const x = e.clientX - 50;
+		const y = e.clientY - 50;
+		element.style.position = "absolute";
+		element.style.left = `${x}px`;
+		element.style.top = `${y}px`;
+		
+		activePiece = element;
+	}
+}
+
+function movePiece(e:React.MouseEvent){
+
+	if(activePiece){
+		const x = e.clientX - 50;
+		const y = e.clientY - 50;
+		activePiece.style.position = "absolute";
+		activePiece.style.left = `${x}px`;
+		activePiece.style.top = `${y}px`;
+	}
+}
+
+function dropPiece(e:React.MouseEvent){
+	if(activePiece) {
+		activePiece = null;
+	}
+}
 
 export default function Chessboard(){
 	let board = [];
@@ -51,9 +85,17 @@ export default function Chessboard(){
 				image = p.image;
 			}
 		})
-		board.push(<Tile image={image} number = {number}  />);
+		
+		
+		board.push(<Tile key = {`${j},${i}`} image={image} number = {number}  />);
 
 		}
 	}
-return <div id="chessboard">{board}</div>;
+return <div 
+	onMouseUp={(e) => dropPiece(e)}
+	onMouseMove = {(e) => movePiece(e)} 
+	onMouseDown = {e => grabPiece(e)} 
+	id="chessboard">
+		{board}
+		</div>;
 }
