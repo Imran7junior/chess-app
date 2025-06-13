425b0a8dded6e2125a6a3d99980ac54c949cd0f5
MD Imran <md.imran.5682@gmail.com>
2025-06-13 14:29:56 +0530

    Update

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -1,22 +1,22 @@
-/* eslint-disable jsx-a11y/alt-text */
-import './Tile.css';
+import "./Tile.css";
 
 interface Props {
-	image?: string;
-	number: number;
+  image?: string;
+  number: number;
+  highlight: boolean;
 }
 
-export default function Tile({ number, image }: Props){
-	if(number % 2 === 0){
-		return <div className='tile black-tile'>
-			{image && <div className='chess-piece' style={{backgroundImage: `url(${image})`}}>
-		</div>}
-		</div>;
-	}
-	else{
-		return <div className='tile white-tile'>
-			{image && <div className='chess-piece' style={{backgroundImage: `url(${image})`}}>
-				</div>}
-		</div>;
-		}
+export default function Tile({ number, image, highlight }: Props) {
+  const className: string = ["tile",
+    number % 2 === 0 && "black-tile",
+    number % 2 !== 0 && "white-tile",
+    highlight && "tile-highlight",
+    image && "chess-piece-tile"].filter(Boolean).join(' ');
+
+
+  return (
+    <div className={className}>
+      {image && <div style={{ backgroundImage: `url(${image})` }} className="chess-piece"></div>}
+    </div>
+  );
 }
